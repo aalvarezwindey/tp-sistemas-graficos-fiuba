4efe78f8c35a90d97e7662c3ba3c8e36e38bdb55
aalvarezwindey <ajalvarez@fi.uba.ar>
2020-06-12 20:19:40 -0300

    update circulo

--- src/geometria/superficie_barrido/poligonos/circulo.js
+++ src/geometria/superficie_barrido/poligonos/circulo.js
@@ -17,22 +17,30 @@ class Circulo extends Poligono {
         0
       )
 
-
-      vertice.normal = vec3.fromValues(...vertice.posicion);
-      vec3.normalize(vertice.normal, vertice.normal);
-
+      // calculamos la tangente como la derivada de la posición
+      // T = r'(t)
       vertice.tangente = vec3.fromValues(
         -1 * radio * _2pi * Math.sin(_2pi * u),
         radio * _2pi * Math.cos(_2pi * u),
         0
       );
+      vec3.normalize(vertice.tangente, vertice.tangente);
 
-      const tangenteX = -1 * _2pi * radio * radio * Math.sin(_2pi * u) * Math.cos(_2pi * u);
-      vertice.binormal = vec3.fromValues(
-        tangenteX,
-        -tangenteX,
+      // calculamos la derivada segunda
+      const derivadaSegunda = vec3.fromValues(
+        -1 * radio * _2pi * _2pi * Math.cos(_2pi * u),
+        -1 * radio * _2pi * _2pi * Math.sin(_2pi * u),
         0
-      );
+      )
+      vec3.normalize(derivadaSegunda, derivadaSegunda)
+
+      // calculamos la binormal como B = T x r''(u)
+      vec3.cross(vertice.binormal, vertice.tangente, derivadaSegunda);
+      vec3.normalize(vertice.binormal, vertice.binormal)
+
+      // calculamos la normal como N = B x T
+      vec3.cross(vertice.normal, vertice.binormal, vertice.tangente);
+      vec3.normalize(vertice.normal, vertice.normal)
 
       this.vertices.push(vertice);
     }
