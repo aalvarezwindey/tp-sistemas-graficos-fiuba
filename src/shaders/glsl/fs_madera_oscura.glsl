57e5aa38b7d068a69f287208965a268f8b78541f
aalvarezwindey <ajalvarez@fi.uba.ar>
2020-06-15 01:55:57 -0300

    actualizo color madera oscura

--- src/shaders/glsl/fs_madera_oscura.glsl
+++ src/shaders/glsl/fs_madera_oscura.glsl
@@ -6,7 +6,7 @@ varying vec2 vUv;
 void main(void) {
     vec3 lightVec = normalize(vec3(0.0, 3.0, 5.0) - vPosWorld);
     vec3 diffColor = mix(vec3(0.7, 0.7, 0.7), vNormal, 0.4);
-    vec3 color = dot(lightVec, vNormal) * diffColor + vec3(0.498, 0.396, 0.30588);
+    vec3 color = dot(lightVec, vNormal) * diffColor + vec3(65.0/255.0, 44.0/255.0, 45.0/255.0);
 
     gl_FragColor = vec4(color, 1.0);
 }
